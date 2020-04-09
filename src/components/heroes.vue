954b52e28190fe970e7fc5517f6cbbb54f4ca648
CuneytYilmaz <cuneyilmaz@gmail.com>
2020-04-09 23:56:29 +0300

    Styles and hero component edited

--- src/components/heroes.vue
+++ src/components/heroes.vue
@@ -4,81 +4,57 @@
       <h2 class="title">Heroes</h2>
     </div>
     <div class="columns">
-      <div class="column is-8">
-        <div class="card edit-detail">
-          <header class="card-header">
-            <p class="card-header-title">{{ hero.firstName }}</p>
-          </header>
-          <div class="card-content">
-            <div class="content">
-              <div class="field">
-                <label class="label" for="id">id</label>
-                <label class="input" id="id" readonly>{{ hero.id }}</label>
-              </div>
-              <div class="field">
-                <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" v-model="hero.firstName" />
-              </div>
-              <div class="field">
-                <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" v-model="hero.lastName" />
-              </div>
-              <div class="field">
-                <label class="label" for="description">description</label>
-                <textarea class="input" id="description" type="text" v-model="hero.description" />
-              </div>
-              <div class="field">
-                <label class="label">cape color</label>
-                <label class="radio" for="color-red">
-                  <input type="radio" id="color-red" value="red" v-model="hero.capeColor" />
-                  red
-                </label>
-                <label class="radio" for="color-blue">
-                  <input type="radio" id="color-blue" value="blue" v-model="hero.capeColor" />
-                  blue
-                </label>
-                <label class="radio" for="color-green">
-                  <input type="radio" id="color-green" value="green" v-model="hero.capeColor" />
-                  green
-                </label>
-                <div class="color-line" :style="{ 'background-color': hero.capeColor }" ></div>
-              </div>
-              <div class="field">
-                <label for="power">
-                  super power
-                  <div class="select is-primary">
-                    <select id="power" v-model="hero.power" @keyup.esc="resetPower" 
-                        :class="{ 'invalid': !hero.power}" >
-                      <option disabled value>Please select one</option>
-                      <option>Speed</option>
-                      <option>Flight</option>
-                      <option>Strength</option>
-                      <option>Invisibility</option>
-                    </select>
-                  </div>
-                </label>
-              </div>
-              <div class="field">
-                <label class="checkbox" for="active">
-                  active
-                  <input type="checkbox" class="is-primary" id="active" v-model="hero.active" />
-                </label>
-              </div>
+      <div class="column is-3">
+        <header class="card-header">
+          <p class="card-header-title">heroes list</p>
+        </header>
+        <ul class="list is-hoverable">
+            <li v-for="hero in heroes" :key="hero.id">
+                <a class="list-item">
+                    <span>{{ hero.firstName }}</span>
+                </a>
+            </li>
+        </ul>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-3">
+        <header class="card-header">
+          <p class="card-header-title">{{ selectedHero.firstName }}</p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            <div class="field">
+              <label class="label" for="id">id</label>
+              <label class="input" id="id" readonly>
+                {{ selectedHero.id }}
+              </label>
+            </div>
+            <div class="field">
+              <label class="label" for="firstName">first name</label>
+              <input
+                class="input"
+                id="firstName"
+                v-model="selectedHero.firstName"
+              />
+            </div>
+            <div class="field">
+              <label class="label" for="lastName">last name</label>
+              <input
+                class="input"
+                id="lastName"
+                v-model="selectedHero.lastName"
+              />
+            </div>
+            <div class="field">
+              <label class="label" for="description">description</label>
+              <input
+                class="input"
+                id="description"
+                v-model="selectedHero.description"
+              />
             </div>
           </div>
-          <footer class="card-footer">
-            <button class="link card-footer-item cancel-button" @click="cancelHero">
-              <i class="fas fa-undo"></i>
-              <span>Cancel</span>
-            </button>
-            <button class="link card-footer-item" @click="saveHero">
-              <i class="fas fa-save"></i>
-              <span>Save</span>
-            </button>
-          </footer>
-        </div>
-        <div class="notification is-info">
-            {{ message }}
         </div>
       </div>
     </div>
@@ -89,29 +65,40 @@
 export default {
   name: 'Heroes',
   data() {
-      return {
-          hero: {
-              id: 20,
-              firstName: 'Madelyn',
-              lastName: 'Papa',
-              description: 'the cat whisperer',
-              capeColor: '',
-              power: '',
-              active: true,
-          },
-          message: 'Test',
-      }
-  },
-  methods: {
-      saveHero() {
-        this.message = JSON.stringify(this.hero, null, '\n');
+    return {
+      selectedHero: {
+        id: 111,
+        firstName: '...',
+        lastName: '...',
+        description: '...',
       },
-      cancelHero() {
-        this.message = '';
-      },
-      resetPower() {
-        this.hero.power = '';
-      },
+      heroes: [
+        {
+          id: 10,
+          firstName: 'Ella',
+          lastName: 'Papa',
+          description: 'fashionista',
+        },
+        {
+          id: 20,
+          firstName: 'Madelyn',
+          lastName: 'Papa',
+          description: 'the cat whisperer',
+        },
+        {
+          id: 30,
+          firstName: 'Haley',
+          lastName: 'Papa',
+          description: 'pen wielder',
+        },
+        {
+          id: 40,
+          firstName: 'Landon',
+          lastName: 'Papa',
+          description: 'arc trooper',
+        },
+      ],
+    };
   },
 };
 </script>
