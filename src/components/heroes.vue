468fc2638041cc631cceb46fcf4d58d34144c2b0
CuneytYilmaz <cuneyilmaz@gmail.com>
2020-04-09 23:48:57 +0300

    V-bind, v-Model, @event, interpolation added for this stage

--- src/components/heroes.vue
+++ src/components/heroes.vue
@@ -17,37 +17,38 @@
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" />
+                <input class="input" id="firstName" v-model="hero.firstName" />
               </div>
               <div class="field">
                 <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" />
+                <input class="input" id="lastName" v-model="hero.lastName" />
               </div>
               <div class="field">
                 <label class="label" for="description">description</label>
-                <textarea class="input" id="description" type="text" />
+                <textarea class="input" id="description" type="text" v-model="hero.description" />
               </div>
               <div class="field">
                 <label class="label">cape color</label>
                 <label class="radio" for="color-red">
-                  <input type="radio" id="color-red" value="red" />
+                  <input type="radio" id="color-red" value="red" v-model="hero.capeColor" />
                   red
                 </label>
                 <label class="radio" for="color-blue">
-                  <input type="radio" id="color-blue" value="blue" />
+                  <input type="radio" id="color-blue" value="blue" v-model="hero.capeColor" />
                   blue
                 </label>
                 <label class="radio" for="color-green">
-                  <input type="radio" id="color-green" value="green" />
+                  <input type="radio" id="color-green" value="green" v-model="hero.capeColor" />
                   green
                 </label>
-                <div class="color-line"></div>
+                <div class="color-line" :style="{ 'background-color': hero.capeColor }" ></div>
               </div>
               <div class="field">
                 <label for="power">
                   super power
                   <div class="select is-primary">
-                    <select id="power">
+                    <select id="power" v-model="hero.power" @keyup.esc="resetPower" 
+                        :class="{ 'invalid': !hero.power}" >
                       <option disabled value>Please select one</option>
                       <option>Speed</option>
                       <option>Flight</option>
@@ -60,23 +61,25 @@
               <div class="field">
                 <label class="checkbox" for="active">
                   active
-                  <input type="checkbox" class="is-primary" id="active" />
+                  <input type="checkbox" class="is-primary" id="active" v-model="hero.active" />
                 </label>
               </div>
             </div>
           </div>
           <footer class="card-footer">
-            <button class="link card-footer-item cancel-button">
+            <button class="link card-footer-item cancel-button" @click="cancelHero">
               <i class="fas fa-undo"></i>
               <span>Cancel</span>
             </button>
-            <button class="link card-footer-item">
+            <button class="link card-footer-item" @click="saveHero">
               <i class="fas fa-save"></i>
               <span>Save</span>
             </button>
           </footer>
         </div>
-        <div class="notification is-info">{{ message }}</div>
+        <div class="notification is-info">
+            {{ message }}
+        </div>
       </div>
     </div>
   </div>
@@ -98,6 +101,17 @@ export default {
           },
           message: 'Test',
       }
+  },
+  methods: {
+      saveHero() {
+        this.message = JSON.stringify(this.hero, null, '\n');
+      },
+      cancelHero() {
+        this.message = '';
+      },
+      resetPower() {
+        this.hero.power = '';
+      },
   },
 };
 </script>
