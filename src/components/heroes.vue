c4fd2808b068a35c5e31471e5bf2f0a052277fb4
CuneytYilmaz <cuneyilmaz@gmail.com>
2020-04-09 22:22:09 +0300

    First hero added

--- src/components/heroes.vue
+++ src/components/heroes.vue
@@ -7,13 +7,13 @@
       <div class="column is-8">
         <div class="card edit-detail">
           <header class="card-header">
-            <p class="card-header-title"></p>
+            <p class="card-header-title">{{ hero.firstName }}</p>
           </header>
           <div class="card-content">
             <div class="content">
               <div class="field">
                 <label class="label" for="id">id</label>
-                <label class="input" id="id" readonly></label>
+                <label class="input" id="id" readonly>{{ hero.id }}</label>
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
@@ -76,7 +76,7 @@
             </button>
           </footer>
         </div>
-        <div class="notification is-info"></div>
+        <div class="notification is-info">{{ message }}</div>
       </div>
     </div>
   </div>
@@ -85,5 +85,19 @@
 <script>
 export default {
   name: 'Heroes',
+  data() {
+      return {
+          hero: {
+              id: 20,
+              firstName: 'Madelyn',
+              lastName: 'Papa',
+              description: 'the cat whisperer',
+              capeColor: '',
+              power: '',
+              active: true,
+          },
+          message: 'Test',
+      }
+  },
 };
 </script>
